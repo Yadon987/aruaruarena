996f0ed2f8c169141914cedeeed600d963449b40
Yadon987 <cvg.fhc.66@gmail.com>
2026-03-02 16:28:56 +0900

    fix: E19-01 OGP無効化とAIキー保護を追加 #0
    - OGP画像アップロード後にCloudFront invalidationを試行するサービスを追加
    - Lambda実行ロールへCloudFront invalidation権限とDistribution ID環境変数を追加
    - TerraformでAIキーが空文字で上書きされないよう必須変数へ変更
    - OGPアップロードとCloudFront invalidationのサービススペックを追加

--- backend/app/services/create_cloud_front_invalidation_service.rb
+++ backend/app/services/create_cloud_front_invalidation_service.rb
@@ -1,62 +1,117 @@
 # frozen_string_literal: true
 
-# CloudFront の指定パスを個別 invalidation するサービス
+require 'net/http'
+require 'uri'
+
+# CloudFront の特定パスを個別 invalidation するサービス
 class CreateCloudFrontInvalidationService
-  def self.call(path:, post_id:, client: nil)
-    new(path:, post_id:, client: client || build_client).execute
+  API_ENDPOINT = 'https://cloudfront.amazonaws.com'
+  API_REGION = 'us-east-1'
+
+  def self.call(path:, post_id:)
+    new(path:, post_id:).execute
   end
 
-  def self.build_client
-    Aws::CloudFront::Client.new
-  end
-  private_class_method :build_client
-
-  def initialize(path:, post_id:, client:)
+  def initialize(path:, post_id:)
     @path = path
     @post_id = post_id
-    @client = client
   end
 
   def execute
     return false if distribution_id.empty?
+    return true if invalidation_succeeded?
 
-    @client.create_invalidation(request_params)
-    log_success
-    true
-  rescue Aws::CloudFront::Errors::ServiceError => e
-    Rails.logger.warn(
-      "[CreateCloudFrontInvalidationService] CloudFront invalidation失敗: post_id=#{@post_id}, path=#{@path}, " \
-      "error=#{e.class} - #{e.message}"
-    )
+    write_failure_log(last_response.code)
+    false
+  rescue StandardError => e
+    write_exception_log(e)
     false
   end
 
   private
 
+  def invalidation_succeeded?
+    response = submit_invalidation_request
+    @last_response = response
+    return false unless response.is_a?(Net::HTTPCreated)
+
+    write_success_log
+    true
+  end
+
+  def submit_invalidation_request
+    Net::HTTP.start(request_uri.host, request_uri.port, use_ssl: true) do |http|
+      http.request(build_request)
+    end
+  end
+
+  attr_reader :last_response
+
   def distribution_id
     ENV.fetch('CLOUDFRONT_DISTRIBUTION_ID', '').strip
   end
 
-  def caller_reference
-    "ogp-#{@post_id}-#{Time.current.to_i}"
+  def request_uri
+    URI("#{API_ENDPOINT}/2020-05-31/distribution/#{distribution_id}/invalidation")
   end
 
-  def request_params
-    {
-      distribution_id: distribution_id,
-      invalidation_batch: {
-        paths: {
-          quantity: 1,
-          items: [@path]
-        },
-        caller_reference: caller_reference
-      }
-    }
+  def build_request
+    request = Net::HTTP::Post.new(request_uri)
+    signed_headers.each { |key, value| request[key] = value }
+    request.body = request_body
+    request
   end
 
-  def log_success
+  def signed_headers
+    signer.sign_request(
+      http_method: 'POST',
+      url: request_uri,
+      headers: { 'content-type' => 'application/xml' },
+      body: request_body
+    ).headers
+  end
+
+  def signer
+    Aws::Sigv4::Signer.new(
+      service: 'cloudfront',
+      region: API_REGION,
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      session_token: ENV.fetch('AWS_SESSION_TOKEN', nil)
+    )
+  end
+
+  def request_body
+    <<~XML
+      <InvalidationBatch xmlns="http://cloudfront.amazonaws.com/doc/2020-05-31/">
+        <Paths>
+          <Quantity>1</Quantity>
+          <Items>
+            <Path>#{@path}</Path>
+          </Items>
+        </Paths>
+        <CallerReference>ogp-#{@post_id}-#{Time.current.to_i}</CallerReference>
+      </InvalidationBatch>
+    XML
+  end
+
+  def write_success_log
     Rails.logger.info(
       "[CreateCloudFrontInvalidationService] CloudFront invalidation成功: post_id=#{@post_id}, path=#{@path}"
+    )
+  end
+
+  def write_failure_log(status_code)
+    Rails.logger.warn(
+      "[CreateCloudFrontInvalidationService] CloudFront invalidation失敗: post_id=#{@post_id}, path=#{@path}, " \
+      "status=#{status_code}"
+    )
+  end
+
+  def write_exception_log(error)
+    Rails.logger.warn(
+      "[CreateCloudFrontInvalidationService] CloudFront invalidation例外: post_id=#{@post_id}, path=#{@path}, " \
+      "error=#{error.class} - #{error.message}"
     )
   end
 end
--- backend/spec/services/create_cloud_front_invalidation_service_spec.rb
+++ backend/spec/services/create_cloud_front_invalidation_service_spec.rb
@@ -3,41 +3,64 @@
 require 'rails_helper'
 
 RSpec.describe CreateCloudFrontInvalidationService, type: :service, dynamodb: false do
-  let(:client) { instance_double(Aws::CloudFront::Client) }
+  let(:signer) { instance_double(Aws::Sigv4::Signer) }
+  let(:signed_request) { instance_double(Aws::Sigv4::Signature, headers: { 'Authorization' => 'signed' }) }
+  let(:http_client) { instance_double(Net::HTTP) }
+  let(:http_response) { instance_double(Net::HTTPCreated, code: '201') }
 
   after do
     ENV.delete('CLOUDFRONT_DISTRIBUTION_ID')
+    ENV.delete('AWS_ACCESS_KEY_ID')
+    ENV.delete('AWS_SECRET_ACCESS_KEY')
+    ENV.delete('AWS_SESSION_TOKEN')
   end
 
   it 'distribution id 未設定時は invalidation を行わないこと' do
-    expect(client).not_to receive(:create_invalidation)
+    expect(Net::HTTP).not_to receive(:start)
 
-    expect(described_class.call(path: '/ogp/posts/post-id.png', post_id: 'post-id', client: client)).to be false
+    expect(described_class.call(path: '/ogp/posts/post-id.png', post_id: 'post-id')).to be false
   end
 
-  it 'CloudFront の invalidation に成功した場合は true を返すこと' do
+  it 'distribution id が設定されている場合は CloudFront invalidation を行うこと' do
     ENV['CLOUDFRONT_DISTRIBUTION_ID'] = 'DIST123'
-    allow(client).to receive(:create_invalidation)
+    ENV['AWS_ACCESS_KEY_ID'] = 'access-key'
+    ENV['AWS_SECRET_ACCESS_KEY'] = 'secret-key'
+    ENV['AWS_SESSION_TOKEN'] = 'session-token'
 
-    expect(described_class.call(path: '/ogp/posts/post-id.png', post_id: 'post-id', client: client)).to be true
-    expect(client).to have_received(:create_invalidation).with(
+    allow(Aws::Sigv4::Signer).to receive(:new).and_return(signer)
+    allow(signer).to receive(:sign_request).and_return(signed_request)
+    allow(Net::HTTP).to receive(:start).and_yield(http_client)
+    allow(http_client).to receive(:request).and_return(http_response)
+    allow(http_response).to receive(:is_a?).with(Net::HTTPCreated).and_return(true)
+
+    expect(described_class.call(path: '/ogp/posts/post-id.png', post_id: 'post-id')).to be true
+
+    expect(Aws::Sigv4::Signer).to have_received(:new).with(
+      service: 'cloudfront',
+      region: 'us-east-1',
+      access_key_id: 'access-key',
+      secret_access_key: 'secret-key',
+      session_token: 'session-token'
+    )
+    expect(signer).to have_received(:sign_request).with(
       hash_including(
-        distribution_id: 'DIST123',
-        invalidation_batch: hash_including(
-          paths: {
-            quantity: 1,
-            items: ['/ogp/posts/post-id.png']
-          }
-        )
+        http_method: 'POST',
+        headers: { 'content-type' => 'application/xml' },
+        body: include('/ogp/posts/post-id.png')
       )
     )
+    expect(http_client).to have_received(:request).with(instance_of(Net::HTTP::Post))
   end
 
-  it 'CloudFront の invalidation に失敗した場合は false を返すこと' do
+  it 'invalidation 中に例外が発生した場合は false を返すこと' do
     ENV['CLOUDFRONT_DISTRIBUTION_ID'] = 'DIST123'
-    error = Aws::CloudFront::Errors::AccessDenied.new(nil, 'access denied')
-    allow(client).to receive(:create_invalidation).and_raise(error)
+    ENV['AWS_ACCESS_KEY_ID'] = 'access-key'
+    ENV['AWS_SECRET_ACCESS_KEY'] = 'secret-key'
 
-    expect(described_class.call(path: '/ogp/posts/post-id.png', post_id: 'post-id', client: client)).to be false
+    allow(Aws::Sigv4::Signer).to receive(:new).and_return(signer)
+    allow(signer).to receive(:sign_request).and_return(signed_request)
+    allow(Net::HTTP).to receive(:start).and_raise(StandardError, 'timeout')
+
+    expect(described_class.call(path: '/ogp/posts/post-id.png', post_id: 'post-id')).to be false
   end
 end
