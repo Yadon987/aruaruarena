d0f0787d6ede320d6acbc0da4be206258b45afb2
Yadon987 <cvg.fhc.66@gmail.com>
2026-03-02 11:44:13 +0900

    fix: E20-03 CI用DynamoDB接続先の初期化を修正 #0
    - specのDynamoDB初期化で環境変数のエンドポイントを優先
    - DynamoDB Local起動直後の接続待ちとしてリトライを追加

--- backend/spec/support/dynamoid.rb
+++ backend/spec/support/dynamoid.rb
@@ -3,18 +3,30 @@
 # DynamoDB Localの設定
 RSpec.configure do |config|
   config.before(:suite) do
-    ENV['DYNAMODB_ENDPOINT'] = 'http://127.0.0.1:8002'
+    endpoint = ENV['DYNAMODB_ENDPOINT'].presence || 'http://127.0.0.1:8002'
+    ENV['DYNAMODB_ENDPOINT'] = endpoint
 
     # テスト用DynamoDB設定
     Dynamoid.configure do |dynamoid_config|
-      dynamoid_config.endpoint = 'http://127.0.0.1:8002'
+      dynamoid_config.endpoint = endpoint
       dynamoid_config.namespace = 'aruaruarena_test'
       dynamoid_config.warn_on_scan = false
       dynamoid_config.read_capacity = 5
       dynamoid_config.write_capacity = 5
     end
 
-    existing_tables = Dynamoid.adapter.list_tables
+    existing_tables = nil
+    attempts = 0
+
+    begin
+      existing_tables = Dynamoid.adapter.list_tables
+    rescue Seahorse::Client::NetworkingError, Errno::ECONNREFUSED => e
+      attempts += 1
+      raise e if attempts >= 10
+
+      sleep 1
+      retry
+    end
 
     [Post, Judgment, RateLimit].each do |model|
       next if existing_tables.include?(model.table_name)
